521aecce6565a11e374c396e6130c0583c94d676
duyhehehe <112858967+duyhehehe@users.noreply.github.com>
2022-09-16 12:06:28 +0700

    Add files via upload

---
+++ SimpleTextEditorHackerrank/src/Solution.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String s = "";
+        Stack<StringBuilder> prev = new Stack<>();
+        int q;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+        q = Integer.parseInt(br.readLine());
+        for (int i = 0; i < q; ++i) {
+            String temp[] = br.readLine().split(" ");
+            if (temp[0].equals("1")) {
+                prev.push(new StringBuilder(stringBuilder.toString()));
+                stringBuilder.append(temp[1]);
+            }
+            if (temp[0].equals("2")) {
+                prev.push(new StringBuilder(stringBuilder.toString()));
+                stringBuilder.delete(stringBuilder.length() - Integer.parseInt(temp[1]), stringBuilder.length());
+            }
+            if (temp[0].equals("3")) {
+                System.out.println(stringBuilder.charAt(Integer.parseInt(temp[1]) - 1));
+            }
+            if (temp[0].equals("4")) {
+                if (!prev.isEmpty()) {
+                    stringBuilder = prev.pop();
+                }
+            }
+
+        }
+    }
+}
